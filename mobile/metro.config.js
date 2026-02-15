8fd3b6e441a43e6f46b14ed77308030dcd2aafb4
Romano98-dev <luca.caterini@outlook.it>
2026-02-15 11:26:59 +0100

    c

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const config = getDefaultConfig(__dirname);
 
 config.resolver.extraNodeModules = {
+    // Polyfills per i moduli Node richiesti erroneamente
     url: require.resolve('url/'),
     crypto: require.resolve('expo-crypto'),
     http: require.resolve('stream-http'),
@@ -11,16 +12,15 @@ config.resolver.extraNodeModules = {
     buffer: require.resolve('buffer/'),
     stream: require.resolve('stream-browserify'),
     path: require.resolve('path-browserify'),
-    // Moduli Node che mandano in crash la build
+    // Reindirizzamento forzato di Axios alla versione corretta
+    axios: path.resolve(__dirname, 'node_modules/axios/dist/axios.js'),
+    // Altri moduli Node da ignorare
     http2: false,
     zlib: false,
     fs: false,
-    net: false,
-    tls: false,
-    child_process: false,
 };
 
-// Forza Metro a ignorare la versione "node" di axios se presente
-config.resolver.blacklistRE = /node_modules\/axios\/dist\/node\/.*/;
+// Rimuovi la blacklistRE se l'avevi messa, usiamo l'alias sopra che è più potente
+config.resolver.blacklistRE = null;
 
 module.exports = config;
